9e1008f78dc87dae87727fbe4bcdbcc8c60d8aee
Pranav Raj <pranavraj273@gmail.com>
2025-11-05 12:59:14 +0530

    Refactor book data and rendering functions

--- shop.js
+++ shop.js
@@ -1,25 +1,25 @@
 
 const books = [
-  { title: "The Power of Habit", author: "Charles Duhigg", format: "Paperback", price: 250.52, offer: "Buy 1 Get 1 50% Off", image: "images/root1.jpg" },
-  { title: "Deep Work", author: "Cal Newport", format: "Paperback", price: 125, offer: "Buy 1 Get 1 50% Off", image: "images/root2.jpg" },
-  { title: "Start With Why", author: "Simon Sinek", format: "Paperback", price: 344.54,offer: "Buy 1 Get 1 50% Off", image: "images/root3.jpg" },
-  { title: "Atomic Habits", author: "James Clear", format: "Paperback", price: 156.02, offer: "Buy 1 Get 1 50% Off", image: "images/root4.jpg" },
-  { title: "Think and Grow Rich", author: "Napoleon Hill", format: "Paperback", price: 417.56, offer: "Buy 1 Get 1 50% Off", image: "images/root5.jpg" },
-  { title: "The Psychology of Money", author: "Morgan Housel", format: "Paperback", price: 461.32, offer: "Buy 1 Get 1 50% Off", image: "images/root61.jpeg" },
-  { title: "Ikigai", author: "Francesc Miralles", format: "Paperback", price: 362.80, offer: "Buy 1 Get 1 50% Off", image: "images/root6.jpg" },
-  { title: "Rich Dad Poor Dad", author: "Robert Kiyosaki", format: "Paperback", price: 23.92, offer: "Buy 1 Get 1 50% Off", image: "images/root7.jpeg" },
-  { title: "Can't Hurt Me", author: "David Goggins", format: "Paperback", price: 363.37, offer: "Buy 1 Get 1 50% Off", image: "images/root8.jpg" },
-  { title: "The Alchemist", author: "Paulo Coelho", format: "Paperback", price: 336.04, offer: "Buy 1 Get 1 50% Off", image: "images/root9.jpg" },
-  { title: "Zero to One", author: "Peter Thiel", format: "Paperback", price: 179.16, offer: "Buy 1 Get 1 50% Off", image: "images/root10.jpg" },
-  { title: "The Subtle Art of Not Giving a F*ck", author: "Mark Manson", format: "Paperback", price: 217.41, offer: "Buy 1 Get 1 50% Off", image: "images/root11.jpeg" },
-  { title: "The 5 AM Club", author: "Robin Sharma", format: "Paperback", price: 440.46, offer: "Buy 1 Get 1 50% Off", image: "images/root12a.jpg" },
-  { title: "Sapiens", author: "Yuval Noah Harari", format: "Paperback", price: 1112.70, offer: "Buy 1 Get 1 50% Off", image: "images/root12.jpg" },
-  { title: "Educated", author: "Tara Westover", format: "Paperback", price: 2634.44, offer: "Buy 1 Get 1 50% Off", image: "images/root13.jpg" },
-  { title: "The Intelligent Investor", author: "Benjamin Graham", format: "Paperback", price: 36.92, offer: "Buy 1 Get 1 50% Off", image: "images/root14.jpg" },
-  { title: "Hooked", author: "Nir Eyal", format: "Paperback", price: 473.23, offer: "Buy 1 Get 1 50% Off", image: "images/root15.jpg" },
-  { title: "Good to Great", author: "Jim Collins", format: "Paperback", price: 189.40, offer: "Buy 1 Get 1 50% Off", image: "images/root16.jpg" },
-  { title: "Man's Search for Meaning", author: "Viktor E. Frankl", format: "Paperback", price: 164.14, offer: "Buy 1 Get 1 50% Off", image: "images/root17.jpg" },
-  { title: "Grit", author: "Angela Duckworth", format: "Paperback", price: 355.68, offer: "Buy 1 Get 1 50% Off", image: "images/root18.jpg" }
+  { title: "The Power of Habit", author: "Charles Duhigg", format: "Paperback", price: 250.52, offer: "Buy 1 Get 1 50% Off", image: "root1.jpg" },
+  { title: "Deep Work", author: "Cal Newport", format: "Paperback", price: 125, offer: "Buy 1 Get 1 50% Off", image: "root2.jpg" },
+  { title: "Start With Why", author: "Simon Sinek", format: "Paperback", price: 344.54,offer: "Buy 1 Get 1 50% Off", image: "root3.jpg" },
+  { title: "Atomic Habits", author: "James Clear", format: "Paperback", price: 156.02, offer: "Buy 1 Get 1 50% Off", image: "root4.jpg" },
+  { title: "Think and Grow Rich", author: "Napoleon Hill", format: "Paperback", price: 417.56, offer: "Buy 1 Get 1 50% Off", image: "root5.jpg" },
+  { title: "The Psychology of Money", author: "Morgan Housel", format: "Paperback", price: 461.32, offer: "Buy 1 Get 1 50% Off", image: "root61.jpeg" },
+  { title: "Ikigai", author: "Francesc Miralles", format: "Paperback", price: 362.80, offer: "Buy 1 Get 1 50% Off", image: "root6.jpg" },
+  { title: "Rich Dad Poor Dad", author: "Robert Kiyosaki", format: "Paperback", price: 23.92, offer: "Buy 1 Get 1 50% Off", image: "root7.jpeg" },
+  { title: "Can't Hurt Me", author: "David Goggins", format: "Paperback", price: 363.37, offer: "Buy 1 Get 1 50% Off", image: "root8.jpg" },
+  { title: "The Alchemist", author: "Paulo Coelho", format: "Paperback", price: 336.04, offer: "Buy 1 Get 1 50% Off", image: "root9.jpg" },
+  { title: "Zero to One", author: "Peter Thiel", format: "Paperback", price: 179.16, offer: "Buy 1 Get 1 50% Off", image: "root10.jpg" },
+  { title: "The Subtle Art of Not Giving a F*ck", author: "Mark Manson", format: "Paperback", price: 217.41, offer: "Buy 1 Get 1 50% Off", image: "root11.jpeg" },
+  { title: "The 5 AM Club", author: "Robin Sharma", format: "Paperback", price: 440.46, offer: "Buy 1 Get 1 50% Off", image: "root12a.jpg" },
+  { title: "Sapiens", author: "Yuval Noah Harari", format: "Paperback", price: 1112.70, offer: "Buy 1 Get 1 50% Off", image: "root12.jpg" },
+  { title: "Educated", author: "Tara Westover", format: "Paperback", price: 2634.44, offer: "Buy 1 Get 1 50% Off", image: "root13.jpg" },
+  { title: "The Intelligent Investor", author: "Benjamin Graham", format: "Paperback", price: 36.92, offer: "Buy 1 Get 1 50% Off", image: "root14.jpg" },
+  { title: "Hooked", author: "Nir Eyal", format: "Paperback", price: 473.23, offer: "Buy 1 Get 1 50% Off", image: "root15.jpg" },
+  { title: "Good to Great", author: "Jim Collins", format: "Paperback", price: 189.40, offer: "Buy 1 Get 1 50% Off", image: "root16.jpg" },
+  { title: "Man's Search for Meaning", author: "Viktor E. Frankl", format: "Paperback", price: 164.14, offer: "Buy 1 Get 1 50% Off", image: "root17.jpg" },
+  { title: "Grit", author: "Angela Duckworth", format: "Paperback", price: 355.68, offer: "Buy 1 Get 1 50% Off", image: "root18.jpg" }
 ];
 
 
@@ -114,3 +114,4 @@ function searchBooks() {
   );
   renderBooks(filtered);
 }
+
